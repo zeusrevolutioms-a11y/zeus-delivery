3b5c118532bd03eb94d2fc5f95046ee9602d7e61
zeusrevolutioms-a11y <zeusrevolutioms@gmail.com>
2026-04-01 11:41:26 -0300

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 
 const app = express();
+app.get('/motoboy', (req, res) => {
+    res.sendFile(__dirname + '/public/motoboy/index.html');
+});
 
+app.get('/motoboy/index.html', (req, res) => {
+    res.sendFile(__dirname + '/public/motoboy/index.html');
+});
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
